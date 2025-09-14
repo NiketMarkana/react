730a1f9c7ce9519c263c164ad1a7b4e24ba971a4
Niket Markana <niketmarkanartj6@gmail.com>
2025-09-14 18:06:52 +0530

    upgrade

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <MyApp /> */}
+
+    {/* ReactElement */} 
+    {/* itwill not work  */}
   </StrictMode>,
 )
