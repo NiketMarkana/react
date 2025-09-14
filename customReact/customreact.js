9f17f1471d0949516fd18c267b443c619939a12a
Niket Markana <niketmarkanartj6@gmail.com>
2025-09-14 17:21:02 +0530

    to know how DOM works in tree

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,12 +1,31 @@
-import { Children } from "react"
+function customRender(reactElement, container){
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
 
- const reactElement = {
-    type : 'a',
-    props : {
-        href : 'https://google.com',
-        target : '_blank'
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
     },
-    Children : 'Click me to visit Google'
- }
- 
- const mainContainer =  document.querySelector('#root')
+    children: 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
