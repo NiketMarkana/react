6cd1cf000bfd64e3f136fcd1d89b39839443dae9
Niket Markana <niketmarkanartj6@gmail.com>
2025-09-16 00:37:21 +0530

    lec

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter]  = useState(15)
+
+  //let counter = 15
+
+  const addValue = () => {
+
+    //counter = counter + 1
+    // it will not work
+    /*if(counter<0) counter = 0
+    setCounter(counter)*/
+
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1 )
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    
+  }
+
+  const removeValue = () => {
+    // it will not work
+    // if(counter>20) counter = 'above 20 is not allowed'
+    setCounter(counter - 1)
+  }
+  
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
